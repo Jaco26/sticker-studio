a394c8c00c2a86aba50058de0fca72a7f9f5bfad
Jacob Albright <jacob.albright23@gmail.com>
2021-10-10 16:13:01 -0500

    implemented proof of concept state machines to change background color and draw line

--- main.js
+++ main.js
@@ -1,40 +1,193 @@
 import { setupDOMRefs } from './lib/dom-refs.js'
 import { setupCanvas } from './lib/canvas.js'
 import { createRect } from './lib/drawing.js'
-
-setupDOMRefs({
-    elems: {
-        container: '.side-bar',
-        menuTrigger: '.header__menu-trigger',
-        content: '.side-bar__content',
-        main: '.main'
-    },
-    events: {
-        menuTrigger: [
-            'click',
-            (elems, evt) => {
-                const action = elems.content.classList.contains('side-bar__content--active')
-                    ? 'remove'
-                    : 'add'
-                elems.content.classList[action]('side-bar__content--active')
-                elems.menuTrigger.classList[action]('header__menu-trigger--active')
-                elems.main.classList[action]('main--side-bar-active')
-            }
-        ]
-    }
-})
+import { setupElements } from './lib/elements.js'
+import { setupEvents } from './lib/events.js'
 
 const canvas = setupCanvas({ containerID: 'canvas' })
 
-const elements = []
+const backgroundState = {
+    mode: 'BLUE',
+    options: {
+        BLUE: {
+            update(main, overlay) {
+                main.clearRect(0, 0, canvas.width(), canvas.height())
+                main.beginPath()
+                main.fillStyle = 'blue'
+                main.fillRect(0, 0, canvas.width(), canvas.height())
+                main.closePath()
+                this.mode = 'ORANGE'
+            },
+        },
+        ORANGE: {
+            update(main, overlay) {
+                main.clearRect(0, 0, canvas.width(), canvas.height())
+                main.beginPath()
+                main.fillStyle = 'orange'
+                main.fillRect(0, 0, canvas.width(), canvas.height())
+                main.closePath()
+                this.mode = 'BLUE'
+            }
+        }
+    }
+}
 
-canvas.on('mousedown', e => {
-    canvas.clear()
-    const rect = createRect(e.offsetX - 25, e.offsetY - 25, 50, 50)
-    rect.fillStyle('orange')
-    rect.strokeStyle('blue')
-    elements.push(rect)
-    canvas.draw(ctx => {
-        elements.forEach(elem => elem.draw(ctx))
+
+const lineState = {
+    mode: 'START',
+    x: null,
+    y: null,
+    options: {
+        START: {
+            handleInput(x, y) {
+                this.x = x
+                this.y = y
+            },
+            update(main, overlay) {
+                overlay.beginPath()
+                overlay.strokeStyle = 'white'
+                overlay.moveTo(this.x, this.y)
+                this.mode = 'DRAWING'
+            }
+        },
+        DRAWING: {
+            handleInput(x, y) {
+                this.x = x
+                this.y = y
+            },
+            update(main, overlay) {
+                overlay.lineTo(this.x, this.y)
+                overlay.stroke()
+            }
+        },
+        FINISH: {
+            handleInput(x, y) {
+                this.x = x
+                this.y = y
+            },
+            update(main, overlay) {
+                overlay.lineTo(this.x, this.y)
+                overlay.closePath()
+                this.mode = 'START'
+                this.x = null
+                this.y = null
+            }
+        },
+    }
+}
+
+
+canvas.on('mousedown', (e) => {
+    
+    canvas.draw((main, overlay) => {
+
+        backgroundState.options[backgroundState.mode].update.call(backgroundState, main, overlay)
+
+        lineState.options[lineState.mode].handleInput.call(lineState, e.offsetX, e.offsetY)
+
+        lineState.options[lineState.mode].update.call(lineState, main, overlay)
+
+    })
+
+    canvas.on('mousemove', e => {
+
+        canvas.draw((main, overlay) => {
+
+            lineState.options[lineState.mode].handleInput.call(lineState, e.offsetX, e.offsetY)
+
+            lineState.options[lineState.mode].update.call(lineState, main, overlay)
+
+        })
+
     })
 })
+
+canvas.on('mouseup', e => {
+
+    canvas.off('mousemove')
+
+    canvas.draw((main, overlay) => {
+
+        overlay.closePath()
+
+        backgroundState.options[backgroundState.mode].update.call(backgroundState, main, overlay)
+
+        lineState.mode = 'FINISH'
+
+        lineState.options[lineState.mode].handleInput.call(lineState, e.offsetX, e.offsetY)
+
+        lineState.options[lineState.mode].update.call(lineState, main, overlay)
+
+    })
+})
+
+
+
+
+// const elements = setupElements()
+
+// const events = setupEvents()
+
+// events.on('NEW_RECT', () => {
+//     const cvsWidth = canvas.width()
+//     const cvsHeight = canvas.height()
+//     const rect = createRect(
+//         cvsWidth - (cvsWidth / 2 + 50),
+//         cvsHeight - (cvsHeight / 2 + 50),
+//         100,
+//         100
+//     )
+//     rect.strokeStyle('#333')
+//     elements.addElement(rect)
+// })
+
+// events.on('TRY_SELECT', () => {
+
+// })
+
+
+// function draw() {
+//     canvas.draw((main, overlay) => {
+//         elements.getElements().forEach(elem => elem.draw(main))
+//     })
+// }
+
+
+// canvas.on('mousedown', e => {
+//     const elem = elements.trySelect(e.offsetX, e.offsetY)
+//     console.log(elem)
+
+// })
+
+// setupDOMRefs({
+//     elems: {
+//         container: '.side-bar',
+//         menuTrigger: '.header__menu-trigger',
+//         content: '.side-bar__content',
+//         main: '.main',
+//         btnCircle: '#create-circle',
+//         btnRect: '#create-rectangle'
+//     },
+//     events: {
+//         menuTrigger: [
+//             'click',
+//             (elems, evt) => {
+//                 const action = elems.content.classList.contains('side-bar__content--active')
+//                     ? 'remove'
+//                     : 'add'
+//                 elems.content.classList[action]('side-bar__content--active')
+//                 elems.menuTrigger.classList[action]('header__menu-trigger--active')
+//                 elems.main.classList[action]('main--side-bar-active')
+//             }
+//         ],
+//         btnRect: [
+//             'click',
+//             () => {
+//                 events.emit('NEW_RECT')
+//                 events.flush()
+//                 draw()
+//             }
+//         ]
+//     }
+// })
+
